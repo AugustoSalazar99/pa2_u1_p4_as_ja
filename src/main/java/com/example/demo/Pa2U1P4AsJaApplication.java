4233b737b36bcb4cc02e783ff0dbfc868770d295
augusto <augusto31115@gmail.com>
2023-05-12 19:40:20 -0500

    luego del almuerzo

--- src/main/java/com/example/demo/Pa2U1P4AsJaApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4AsJaApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U1P4AsJaApplication {
+public class Pa2U1P4AsJaApplication implements  CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P4AsJaApplication.class, args);
+		
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Mi primer proyecto");
+		/*
+		 * Spring Framework: framework que permite la construccion/ 
+		 * desarrollo de aplicaciones empresariales basadas en Spring
+		 */
 	}
 
 }
