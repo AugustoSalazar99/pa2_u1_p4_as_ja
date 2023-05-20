19b8d25c4e45e41a919450e6997250108dffff6f
augusto <augusto31115@gmail.com>
2023-05-19 19:42:39 -0500

    Taller6

---
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+public interface EstudianteRepository {
+	// methCRUD
+	public void insertar(Estudiante estudiante);
+
+	public void actualizar(Estudiante estudiante);
+
+	// se busca por un identificador
+	public Estudiante seleccionar(String cedula);
+
+	// se busca por un identificador
+	public void eliminar(String cedula);
+
+}
